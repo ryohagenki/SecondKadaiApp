c2100ce04235f77f91be18582822dd936813556a
Ryo <yanano1228@gmail.com>
2022-04-22 20:05:57 +0900

    あいさつアプリを作成。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 小柳崚 on 2022/04/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var greetingLabel: UILabel!
+    var nameData = ""
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.text = "こんにちは、\(nameData)さん"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toNext" {
+            let nextView = segue.destination as! ResultViewController
+            nextView.nameData = nameTextField.text!
+        }
+    }
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
 
 }
 
+    
